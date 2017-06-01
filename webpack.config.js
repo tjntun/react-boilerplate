a04995f32e343dca8a8d16d10a9bbadbc9cbab5a
sydinh <sydinh.dev@gmail.com>
2017-06-01 17:14:50 +0700

    first commit :muscle:

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const SOURCE_DIR = path.resolve(__dirname, "./source");
+const BUILD_DIR = path.resolve(__dirname, "./build");
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+  entry: SOURCE_DIR + "/index.js",
+  output: {
+    path: BUILD_DIR,
+    filename: "./index.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: "babel-loader"}
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: "style-loader"},
+          {loader: "css-loader", options: {sourceMap: true}},
+          {loader: "sass-loader", options: {sourceMap: true}}
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    open: true,
+    port: 9000,
+    stats: "errors-only"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./source/index.html",
+      minify: {
+        collapseWhitespace: isProd
+      }
+    })
+  ]
+}
+
+module.exports = config;
