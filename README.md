d5baa6031146cdd3d3189b628503d913e5db4b94
sydinh <sydinh.dev@gmail.com>
2017-06-04 10:13:36 +0700

    update some files :sparkles:

---
+++ README.md
@@ -0,0 +1,25 @@
+# React Redux Boilerplate
+
+## Installing
+
+NodeJS and NPM is required
+
+1. Clone this repo ``git clone https://github.com/sydinh/template-for-react-and-redux.git``.
+2. Run ``npm install`` to get all the Node Packages.
+3. Run ``npm run dev`` for development mode.
+4. New browser window should open automatically.
+
+## Built With
+
+* [Webpack](https://webpack.js.org/) - A module bundler.
+* [Babel](https://babeljs.io/) - A JavaScript compiler.
+
+## Contributing
+
+Please, follow
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b your-new-feature`.
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin your-new-feature`.
+5. Submit a pull request.
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const SOURCE_DIR = path.resolve(__dirname, "./source");
-const BUILD_DIR = path.resolve(__dirname, "./build");
+const SOURCE_DIR = path.resolve(__dirname, './src');
+const BUILD_DIR = path.resolve(__dirname, './build');
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
-  entry: SOURCE_DIR + "/index.js",
+  entry: SOURCE_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
-    filename: "./index.bundle.js"
+    filename: './index.bundle.js'
   },
   module: {
     rules: [
@@ -18,30 +18,29 @@ const config = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-          {loader: "babel-loader"}
+          {loader: 'babel-loader'}
         ]
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: [
-          {loader: "style-loader"},
-          {loader: "css-loader", options: {sourceMap: true}},
-          {loader: "sass-loader", options: {sourceMap: true}}
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: {sourceMap: true}},
+          {loader: 'sass-loader', options: {sourceMap: true}}
         ]
       }
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, "build"),
+    contentBase: path.join(__dirname, 'build'),
     historyApiFallback: true,
     open: true,
-    port: 9000,
-    stats: "errors-only"
+    stats: 'errors-only'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./source/index.html",
+      template: './src/index.html',
       minify: {
         collapseWhitespace: isProd
       }
